65d1137ce2f5878af325d52b5327f020ae535e3a
sahilnair14 <ssn.nair1999@gmail.com>
2026-07-03 13:06:23 +0530

    SNIP_API

--- src/main/java/in/deploywizard/snip/config/CorsConfig.java
+++ src/main/java/in/deploywizard/snip/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origins:http://localhost:5173}")
+    @Value("${app.cors.allowed-origins:https://snip-ui.devopsmafia.live}")
     private String allowedOrigins;
 
     @Override
